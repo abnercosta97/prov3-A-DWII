6572026c35f27792363d1597dcb5bb7b8ba84d81
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2023-12-12 18:48:29 -0300

    arquivos na pasta types e service, como requisitado

--- front/src/services/Color.ts
+++ front/src/services/Color.ts
@@ -1,1 +1,20 @@
-export{}
+import { ColorProps, ResultProps } from "../types";
+import api from "./api";
+class Color {
+  async list(): Promise<ColorProps[]> {
+    const { data } = await api.get("/");
+    return data;
+  }
+  async create(props: ColorProps): Promise<ColorProps> {
+    const { data } = await api.get(
+      `/${props.red}/${props.green}/${props.blue}`
+    );
+    return data;
+  }
+  async remove(id: number): Promise<ResultProps> {
+    const { data } = await api.get(`/${id}`);
+    return data;
+  }
+}
+const color = new Color();
+export default color;
--- front/src/types/index.ts
+++ front/src/types/index.ts
@@ -1,1 +1,9 @@
-export{}
+export interface ColorProps {
+  id?: number;
+  red: number;
+  green: number;
+  blue: number;
+}
+export interface ResultProps {
+  count: number;
+}
